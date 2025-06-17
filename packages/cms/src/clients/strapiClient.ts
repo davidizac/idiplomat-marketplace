eac76bd0a7cf91c1c00a049b5d6d6b1a4a42c5ae
davidizac <davidgah@wix.com>
2025-06-17 21:07:44 +0300

    fix

--- packages/cms/src/clients/strapiClient.ts
+++ packages/cms/src/clients/strapiClient.ts
@@ -9,7 +9,9 @@ import { strapi } from "@strapi/client";
 const STRAPI_URL =
 	process.env.NEXT_PUBLIC_STRAPI_URL ?? "http://localhost:1337";
 const STRAPI_API_URL = `${STRAPI_URL}/api`;
-const STRAPI_TOKEN = process.env.STRAPI_TOKEN ?? "";
+const STRAPI_TOKEN =
+	process.env.STRAPI_TOKEN ??
+	"fdeeddbf6f22beb0b52ef6925226f2c94a0703eeec281a63a2d75f124ff05e8d5280e24e7df4902755c3a5f5de6c17f440f3983d2ad185e997c5eb82d8bd288ff45c52eca3bd09e80ee6e48799fd7ec26e52c7e7c33266376fdfa403eeb0d2ada86e85fd082bfe7bc4f86a54ac775cb0e145d24675aae53bd3d862c4d842f955";
 
 /**
  * Initialize the Strapi client with authentication
